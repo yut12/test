d9d89dccda32872c6d016fcb01bad962633e7164
koko27 <92703257+yamamoto5716@users.noreply.github.com>
2021-11-24 15:10:55 +0900

    Add files via upload

---
+++ blog/forms.py
@@ -0,0 +1,52 @@
+from django import forms
+from django.core.mail import EmailMessage
+from .models import Blog
+
+class InquiryForm(forms.Form):
+    name = forms.CharField(label='名前', max_length=30)
+    email = forms.EmailField(label='メアド')
+    title = forms.CharField(label='タイトル', max_length=30)
+    message = forms.CharField(label='内容', widget=forms.Textarea)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields['name'].widget.attrs['class'] = 'form-control col-9'
+        self.fields['name'].widget.attrs['placeholder'] = '名前をここに入力してください。'
+
+        self.fields['email'].widget.attrs['class'] = 'form-control col-11'
+        self.fields['email'].widget.attrs['placeholder'] = 'メアドをここに入力してください。'
+
+        self.fields['title'].widget.attrs['class'] = 'form-control col-11'
+        self.fields['title'].widget.attrs['placeholder'] = 'タイトルをここに入力してください。'
+
+        self.fields['message'].widget.attrs['class'] = 'form-control col-12'
+        self.fields['message'].widget.attrs['placeholder'] = '内容をここに入力してください。'
+
+    def send_email(self):
+        name = self.cleaned_data['name']
+        email = self.cleaned_data['email']
+        title = self.cleaned_data['title']
+        message = self.cleaned_data['message']
+
+        subject = 'お問い合わせ {}'.format(title)
+        message = '送信者名: {0}\nメールアドレス: {1}\nメッセージ:\n{2}'.format(name, email, message)
+        from_email = 'admin@example.com'
+        to_list = [
+            'test@example.com'
+        ]
+        cc_list = [
+            email
+        ]
+
+        message = EmailMessage(subject=subject, body=message, from_email=from_email, to=to_list, cc=cc_list)
+        message.send()
+
+class BlogCreateForm(forms.ModelForm):
+    class Meta:
+        model = Blog
+        fields = ('title','content','photo1','photo2','photo3',)#下にclassに
+    def __init__(self,*args,**kwargs):
+        super().__init__(*args,**kwargs)
+        for field in self.fields.values():
+            field.widget.attrs['class'] = 'form-control' #タグにクラス属性を追加する
---
+++ company/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+from .models import Company
+
+
+admin.site.register(Company)
+
+# Register your models here.
